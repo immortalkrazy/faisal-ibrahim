cdf91225ef2ae9a6a3d397a37418cb7f0f9a7cb0
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2020-04-26 17:35:17 -0400

    added folders

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById('btn');
+let lnk = document.getElementById('lnk');
+let txt = document.getElementById('txt');
+
+let but = document.querySelector("button");
+
+but.addEventListener('click', function(){
+    alert("Clicked!")
+});
+

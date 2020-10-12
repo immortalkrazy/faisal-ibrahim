25f259d9858c5c36dc3eb180515b75263fef436d
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2020-10-12 01:07:28 -0400

    adding login page

---
+++ Login/connect.php
@@ -0,0 +1,15 @@
+<?php
+
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $phone = $_POST['phone'];
+
+    // Database Connection
+    $conn = new mysqli('local')
+
+
+
+?>
